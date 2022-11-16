92d1bf809107d16dd15d44fd7f965b7e2f529139
ersin7777 <ersin.mustafa@pgmett.com>
2022-11-16 09:11:34 +0200

    added

---
+++ GitExercise/OptionsManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GitExercise
+{
+    public static class OptionsManager
+    {
+        public static string[] OptionsList = {
+            "a - Add",
+            "s - Subtract",
+            "m - Multiply",
+            "d-Divide",
+            "sab - Subtrackt Abs"
+
+        };
+        public static void Divide(double a, double b)
+        {
+            Console.WriteLine($"|{a}:{b}={a / b}");
+        }
+
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+
+        public static void Multiply(double a, double b)
+        {
+            Console.WriteLine($"{a} x {b} = {a * b}");
+        }
+
+        public static void Subtract(double a, double b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+    }
+}
